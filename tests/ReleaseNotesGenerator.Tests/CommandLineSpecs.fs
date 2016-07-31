a3c2c558c2dbd8e43a866556179f39af3aa1e4d2
Anton Sizikov <sizikov.anton@gmail.com>
2016-07-31 18:12:12 +0200

    Add unit-tests project and first CommandLineSpecs unit test

---
+++ tests/ReleaseNotesGenerator.Tests/CommandLineSpecs.fs
@@ -0,0 +1,21 @@
+﻿module CommandLineSpecs
+
+open NUnit.Framework
+open FsUnit
+open CommandLine
+
+
+let getDefaultOptions () =
+    {
+       whatif = false;
+       repository = "";
+       token = "";
+       branch = "master"
+       }
+
+[<Test>]
+let ``branch parameter can be parsed``() = 
+    let defaultOptions = getDefaultOptions()
+    let newBranchName = "newBranch"
+    let args = ["/branch"; newBranchName;]
+    (parseCommandLine args  defaultOptions ).branch |> should equal newBranchName
